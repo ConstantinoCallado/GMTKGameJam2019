629979319d487c6aec2cd04f707f47cce5d4df59
gh-ccallado <49192144+gh-ccallado@users.noreply.github.com>
2019-08-03 00:58:16 +0200

    First prototype of pickup and throwing the orb

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public Transform orbSocket;
+    public float orbThrowSpeed = 10;
+    private Orb orb;
+    private Camera camera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ThrowOrb();
+        }
+    }
+
+    void ThrowOrb()
+    {
+        if(orb)
+        {
+            orb.Throw();
+            orb.transform.parent = null;
+            orb.GetRigidbody().AddForce(camera.transform.forward * orbThrowSpeed, ForceMode.VelocityChange);
+            orb = null;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Orb")
+        {
+            // Pick up the orb
+            orb = other.gameObject.GetComponent<Orb>();
+            orb.PickUp();
+
+            orb.transform.parent = orbSocket;
+            orb.transform.localPosition = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/Orb.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private Collider collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PickUp()
+    {
+        rigidbody.isKinematic = true;
+        collider.enabled = false;
+    }
+
+    public void Throw()
+    {
+        rigidbody.isKinematic = false;
+        collider.enabled = true;
+    }
+
+    public Rigidbody GetRigidbody()
+    {
+        return rigidbody;
+    }
+}
