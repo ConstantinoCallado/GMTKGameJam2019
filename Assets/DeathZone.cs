5ef3caeaf8a6bbcd9bf5d5bc84f8c5fcae80a6bd
LuisM <luismb55@gmail.com>
2019-08-03 23:06:42 +0200

    Adds knockback to enemies

---
+++ Assets/DeathZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    public bool destroyNonPlayerObjects = true;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag is "Player")
+        {
+            // if player then tell the player to do its FallDeath
+            //TO DO
+            //Destroy(gameObject);
+        } else if (destroyNonPlayerObjects)
+        { // not playe so just kill object - could be falling enemy for example
+            Object.Destroy(collision.gameObject);
+        }
+    }
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,57 +1,191 @@
 using System;
 using UnityEngine;
 
-//namespace UnityStandardAssets.Characters.ThirdPerson
-//{
-    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
-    //[RequireComponent(typeof (ThirdPersonCharacter))]
-    public class EnemyController : MonoBehaviour
+public enum EnemyState { Idle, SeekingPlayer, SeekingSpot, Attacking, Knocked, Falling, Dying };
+
+[RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+
+public class EnemyController : MonoBehaviour
+{
+       
+    public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+    public Transform character { get; private set; }
+
+    public LayerMask whatIsGround;
+
+    public Transform target;                                    // target to aim for
+
+    public float maximumSeekDistance;
+
+    // knockback
+    public float knockbackDistance;
+    // Movement speed in units/sec.
+    public float knockbackSpeed = 1.0F;
+
+    public float knockbackTime;
+
+    [Header("Initial state")]
+    public EnemyState initialState;
+
+    Rigidbody m_Rigidbody;
+    Animator m_Animator;
+    EnemyState m_state;
+
+    [SerializeField] float m_GroundCheckDistance = 1f;
+    bool m_IsGrounded;
+    Vector3 m_GroundNormal;
+
+    private float m_knockbackEndTime;
+
+    private void Start()
     {
-        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
-        //public ThirdPersonCharacter character { get; private set; } // the character we are controlling
-        public Transform character { get; private set; }
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Animator = GetComponent<Animator>();
+        m_state = initialState;
 
-        public Transform target;                                    // target to aim for
+        // get the components on the object we need ( should not be null due to require component so no need to check )
+        agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+        character = GetComponent<Transform>();
+        
+        agent.updateRotation = true;
+	    agent.updatePosition = true;
+    }
 
 
-        private void Start()
+    private void Update()
+    {
+        switch (m_state)
         {
-            // get the components on the object we need ( should not be null due to require component so no need to check )
-            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
-            character = GetComponent<Transform>();
+            case EnemyState.Idle:
+                if (target != null)
+                {
+                    float targetDistance = Vector3.Distance(target.transform.position, transform.position);
 
+                    if (targetDistance <= maximumSeekDistance)
+                    {
+                        m_state = EnemyState.SeekingPlayer;
+                        m_Animator.SetBool("playerIsVisible", true);
+                    }
+                }
+                break;
+            case EnemyState.SeekingPlayer:
+                if (target != null)
+                {
+                    float targetDistance = Vector3.Distance(target.transform.position, transform.position);
+                    
+                    if(agent.enabled)
+                        agent.SetDestination(target.position);
 
-            agent.updateRotation = true;
-	        agent.updatePosition = true;
+                    if (targetDistance > maximumSeekDistance)
+                    {
+                        m_state = EnemyState.Idle;
+                        m_Animator.SetBool("playerIsVisible", false);
+                    }
+                }
+
+                break;
+            case EnemyState.Knocked:
+                if (m_knockbackEndTime < Time.time)
+                {
+                    m_Animator.SetBool("knocked", false);
+                    m_Rigidbody.isKinematic = true;
+                    agent.enabled = true;
+                    m_state = EnemyState.Idle;
+                }
+                break;
+            case EnemyState.Falling:
+                if (m_IsGrounded)
+                {
+                    m_Animator.SetBool("falling", false);
+                    m_Rigidbody.isKinematic = true;
+                    agent.enabled = true;
+                    m_state = EnemyState.Idle;
+                }
+                break;
+            case EnemyState.SeekingSpot:
+
+                break;
+            case EnemyState.Dying:
+
+                break;
+            default:
+                break;
         }
 
+        CheckGroundStatus();
 
-        private void Update()
+        if (!m_IsGrounded)
         {
-            // seeking behavior
-            if (target != null)
-                agent.SetDestination(target.position);
-
-            
-            /*if (agent.remainingDistance > agent.stoppingDistance)
-                character.Move(agent.desiredVelocity, false, false);
-            else
-                character.Move(Vector3.zero, false, false);
-                */
-            
+            // Start falling
+            m_Rigidbody.isKinematic = false;
+            agent.enabled = false;
+            m_Animator.SetBool("falling", true);
+            m_state = EnemyState.Falling;
         }
+    }
 
 
-        public void SetTarget(Transform target)
-        {
-            this.target = target;
-        }
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name is "Orb")
-            Destroy(gameObject);
-            
+        {
+            //if (m_state != EnemyState.Knocked)
+            {
+                Vector3 knockback = Vector3.zero;
+
+                foreach (ContactPoint contact in collision.contacts)
+                {
+                    Debug.DrawRay(contact.point, contact.normal, Color.white);
+
+                    knockback.x += contact.normal.x;
+                    knockback.z += contact.normal.z;
+
+                }
+                knockback.Normalize();
+                knockback.Scale(new Vector3(knockbackDistance, knockbackDistance, knockbackDistance));
+
+                Knocked(knockback);
+            }
+
+            //Destroy(gameObject);
+        }
+        
+    }
+
+    void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+#if UNITY_EDITOR
+        // helper to visualise the ground check ray in the scene view
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
+#endif
+        // 0.1f is a small offset to start the ray from inside the character
+        // it is also good to note that the transform position in the sample assets is at the base of the character
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance, whatIsGround))
+        {
+            m_IsGrounded = true;
+        }
+        else
+        {
+            m_IsGrounded = false;
+        }
+    }
+
+    private void Knocked(Vector3 distance)
+    {
+        m_knockbackEndTime = Time.time + knockbackTime;
+
+        m_Rigidbody.isKinematic = false;
+        m_Rigidbody.AddForce(distance, ForceMode.VelocityChange);
+
+        agent.enabled = false;
+
+        m_state = EnemyState.Knocked;
+        m_Animator.SetBool("knocked", true);
     }
 }
-//}
