d9bc3d1c236b0adce185d791b15f1375b8f1d1aa
LuisM <luismb55@gmail.com>
2019-08-03 18:29:02 +0200

    Adds Enemy prototype and Enemy scene

---
+++ Assets/Scripts/Floating.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    float floatY;
+    public float FloatStrength; // Set strength in Unity
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 floatingPosition = transform.position;
+        floatY = transform.position.y;
+        floatY = (Mathf.Sin(Time.time) * FloatStrength);
+        floatingPosition.y += floatY;
+        transform.position = floatingPosition;
+    }
+}
