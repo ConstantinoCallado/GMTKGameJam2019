ef8f3009c2566a3d32e68f860d6546b668cf9637
gh-ccallado <49192144+gh-ccallado@users.noreply.github.com>
2019-08-03 16:15:28 +0200

    Updated some prefabs

---
+++ Assets/Scripts/EnergyContainer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnergyType { None, Light, Damage, Key };
+
+
+
+public class EnergyContainer : MonoBehaviour
+{
+    [Header("Energy settings")]
+    public EnergyType energyType;
+    public float maxEnergyPool = 100f;
+    public float energy = 100f;
+
+    public Renderer orbMesh;
+    
+
+    public void Start()
+    {
+        RecalculateAspect();
+    }
+
+    public void RecalculateAspect()
+    {
+        //orbMesh.material.SetColor ("_EmissionColor", Color.black);
+    }
+}
