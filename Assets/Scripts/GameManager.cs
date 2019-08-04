4abf495a01c366a8b5796c4118be682eece53b0a
gh-ccallado <49192144+gh-ccallado@users.noreply.github.com>
2019-08-04 19:09:43 +0200

    Adde pause menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class GameManager : MonoBehaviour
+{
+    bool gamePaused = false;
+    public GameObject pauseMenu;
+    private FirstPersonController firstPersonController;
+    private Character character;
+    
+    public void Start()
+    {
+        Cursor.visible = false;
+        firstPersonController = GetComponent<FirstPersonController>();
+        character = GetComponent<Character>();
+    }
+
+    public void PauseGame()
+    {
+        firstPersonController.enabled = false;
+        character.enabled = false;
+        gamePaused = true;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        firstPersonController.enabled = true;
+        character.enabled = true;
+        gamePaused = false;
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        pauseMenu.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !gamePaused)
+        {
+            gamePaused = !gamePaused;
+            PauseGame();
+        }
+    }
+}
